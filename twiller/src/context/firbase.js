7c70e0d872a89965018610a35cbca0468125a0a0
Kaipa Suma Bhargav Reddy <137994264+1DS22CS099-SumaBhargavReddy@users.noreply.github.com>
2025-04-13 21:50:45 +0530

    Update firbase.js

--- twiller/src/context/firbase.js
+++ twiller/src/context/firbase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBlykSvq0kyjpryrpOMfCl4kxNZYIy9BI",
-  authDomain: "twiller-twitterclone-2a0e2.firebaseapp.com",
-  projectId: "twiller-twitterclone-2a0e2",
-  storageBucket: "twiller-twitterclone-2a0e2.firebasestorage.app",
-  messagingSenderId: "299553073111",
-  appId: "1:299553073111:web:6fab1ba477c78d68c3cd92"
+  apiKey: 
+  authDomain: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId:
 };
 
 const app = initializeApp(firebaseConfig);
